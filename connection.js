a7e26c60372492516c8f9e58edebb1306e8fab7d
boobalannaresh <bestforevermvi@gmail.com>
2023-07-11 19:23:16 +0530

    Socket error

--- connection.js
+++ connection.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const colors = require('colors');
 
-const connectDB = async() =>{
-    try{
-       await mongoose.connect(process.env.MONGO_URL)
-       console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
-    }catch(error){
-        console.log(`MongoDB Server Issue ${error}` .bgRed.white)
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
+    } catch (error) {
+        console.log(`MongoDB Server Issue ${error}`.bgRed.white)
     }
 }
 
--- server.js
+++ server.js
@@ -9,22 +9,22 @@ const Message = require("./models/Message");
 const User = require("./models/User");
 
 dotenv.config();
-connectDB();
+
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors({
-    origin: "https://chat-app-frontend-lilac.vercel.app"
-}));
+app.use(cors());
 
 app.use("/users", userRoutes);
+connectDB();
 
-const server = require('http').createServer(app);
+const server = require('https').createServer(app);
 const PORT = process.env.PORT;
 const io = require("socket.io")(server, {
     cors: {
         origin: "https://chat-app-frontend-lilac.vercel.app",
+        // origin: "http://localhost:3000",
         methods: ["GET", "POST"]
     }
 })

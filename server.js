08f5aa31a4dce6757715a119adc3b02ecdbe32f6
boobalannaresh <bestforevermvi@gmail.com>
2023-07-11 22:25:15 +0530

    going issue

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const io = require("socket.io")(server, {
     cors: {
         origin: "https://chat-app-frontend-lilac.vercel.app",
         // origin: "http://localhost:3000",
-        // methods: ["GET", "POST"]
+        methods: ["GET", "POST"]
     }
 })
 io.listen(PORT);
